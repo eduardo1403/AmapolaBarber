0011e06363f57f970cda03d3a2e5ac84923a9f91
eduardo1403 <eduardo1403hm@gmail.com>
2023-12-04 02:23:38 -0600

    Update notificaciones.js

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -1,13 +1,3 @@
 function mostrarNotificacion() {
-    if (Notification.permission === 'granted') {
-      const notificacion = new Notification('Nuevos Productos.!', {
-        body: 'Conoce los nuevos productos de la tienda',
-        icon: 'img/noti.jpg'
-      });
-  
-      notificacion.addEventListener('click', () => {
-        // Acción cuando el usuario hace clic en la notificación
         console.log('El usuario hizo clic en la notificación');
-      });
-    }
   }
