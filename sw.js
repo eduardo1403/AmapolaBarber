6ca38b8c78b9fb3c18aba65aa9ff9e7212e6d279
eduardo1403 <eduardo1403hm@gmail.com>
2023-12-04 01:06:01 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 
 self.addEventListener('install', e=>{
-    console.log('Instalando SW')
+    console.log('Instalando SW');
+})
+
+
+self.addEventListener('activate', e=>{
+    console.log('SW: Activo y Controlara toda la aplicación');
 })
