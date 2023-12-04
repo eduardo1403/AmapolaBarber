71e7551db2c36cd1bcc13ecb708fbf6008994dec
eduardo1403 <eduardo1403hm@gmail.com>
2023-12-04 00:51:40 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-if('serviceWorker' in navigator){
+/*if('serviceWorker' in navigator){
     console.log('Podemos usar SW')
 }else{
     console.log('No podemos usar SW')
+}*/
+
+//Registrar Service Worker
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('sw.js')
 }
